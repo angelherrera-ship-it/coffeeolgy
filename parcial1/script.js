55856675faf50c27b7156e2918a62336c89032af
ANNGELO3000 <angelosrielherreratola@gmail.com>
2026-08-03 19:10:20 -0500

    avence script

--- parcial1/script.js
+++ parcial1/script.js
@@ -2,4 +2,5 @@ function mostrarMensaje() {
     const mensaje = document.querySelector("#mensaje");
     mensaje.textContent = "Pedido recibido - te atiendo Angel Herrera";
     mensaje.classList. remove("oculto");}
-    
+const boton = document.querySelector("#btn-confirmar");
+boton.addEventListener("click", mostrarMensaje);
