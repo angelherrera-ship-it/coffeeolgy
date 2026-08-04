770f5df17e339c744674a5dc1b0491d9560dca9b
ANNGELO3000 <angelosrielherreratola@gmail.com>
2026-08-03 19:07:58 -0500

    parcial1

---
+++ parcial1/script.js
@@ -0,0 +1,5 @@
+function mostrarMensaje() {
+    const mensaje = document.querySelector("#mensaje");
+    mensaje.textContent = "Pedido recibido - te atiendo Angel Herrera";
+    mensaje.classList. remove("oculto");}
+    
