c3d619638798ea4fceda9574ad196a8cbbe02e22
ANNGELO3000 <angelosrielherreratola@gmail.com>
2026-08-18 08:31:40 -0500

    corregi rutas

--- mi-tienda/routes/web.php
+++ mi-tienda/routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
 });
 
 Route::get('/dashboard', function () {
